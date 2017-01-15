9d156d8ec5a430f999182812f7dca40627fcbd37
RandomByte <r@ndombyte.net>
2017-01-15 22:31:03 +0100

    Improve error handling

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.post("/alexa", function(req, res) {
 	if (!oMqttClient.connected) {
 		console.log("Failed to connect to MQTT broker");
+		res.status(500);
 		res.send("Failed to connect to MQTT broker");
 		return;
 	}
@@ -33,13 +34,20 @@ app.post("/alexa", function(req, res) {
 		oMqttClient.publish("Home/StateIntents", sIntent, {
 			qos: 2,
 			retain: true
-		}, function() {
-			console.log("Intent published");
-			res.send("Intent published");
+		}, function(err) {
+			if (err) {
+				console.log("MQTT publish failed: " + err);
+				res.status(500);
+				res.send("Internal error");
+			} else {
+				console.log("Intent published");
+				res.send("Intent published");
+			}
 		});
 		break;
 	default:
 		console.log("Unknown intent");
+		res.status(400);
 		res.send("Unknown intent");
 		break;
 	}
